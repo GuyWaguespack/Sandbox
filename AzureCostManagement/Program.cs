a26bf8a0d2dd3e2d17295f5e58ba9959f7d8b959
Guy Waguespack <guy.waguespack@gmail.com>
2019-09-09 10:51:56 -0500

    no message

---
+++ AzureCostManagement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.Azure.Management.Consumption;
+using Microsoft.Azure.Management.Consumption.Models;
+
+using Microsoft.Rest;
+using Microsoft.Rest.Azure;
+
+
+namespace AzureCostManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+//            using (ConsumptionManagementClient client = new ConsumptionManagementClient())
+        }
+    }
+}
