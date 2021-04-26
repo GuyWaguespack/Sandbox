0ae8e4995c4f95efed032cdb8c707415edfa585e
Guy Waguespack <guy.waguespack@gmail.com>
2021-04-26 14:49:24 -0500

    SMTP Example with Gmail

---
+++ Sandbox.Email/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Sandbox.Email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailAddress fromAddress = new MailAddress("xxxxxxxx@company.com", "John Smith");
+            MailAddress toAddress = new MailAddress("xxxxxxxx@company.com", "Jane Doe");
+            const string fromPassword = "xxxxxxxxxxxxxxxx";
+            const string subject = "Test Subject";
+            const string body = "Test Body";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (MailMessage message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
